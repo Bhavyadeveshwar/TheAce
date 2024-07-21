005b8c78829b63e08513eef0d3512b4d803054f9
Bhavyadeveshwar <bhavyadeveshwar@gmail.com>
2024-07-21 16:56:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import About from './Components/About/About'
+import Hero from './Components/Hero/Hero'
+
+function App() {
+  
+
+  return (
+    <>
+  <Hero/>  
+      <About/>
+       </>
+  )
+}
+
+export default App
